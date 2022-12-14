d437939eb591b7bf4152a1dcedb80ec7cf8e540e
Gabriel <55121175+Vannerss@users.noreply.github.com>
2022-12-14 17:23:42 -0500

    worked on ui

---
+++ SilentSpace_EssentialsBuild/Assets/Scripts/Player/Interaction/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using SilentSpace.Player.interaction;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SilentSpace.Player.Interaction
+{
+    public class Notes : MonoBehaviour, IInteractable
+    {
+        public string InteractionPrompt { get; }
+
+        public Button noteButton;
+        
+        public void Interact()
+        {
+            noteButton.interactable = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
