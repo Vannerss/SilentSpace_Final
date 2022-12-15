0878249c766313187598c06c1f68f4f3819268c8
kj432171 <mjmontanob35@gmail.com>
2022-12-14 21:41:31 -0500

    objective
    objective screen test, and objective flags added

---
+++ SilentSpace_EssentialsBuild/Assets/Scripts/Quest manager/Quest_Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SilentSpace
+{
+    public class Quest_Manager : MonoBehaviour
+    {
+        public bool crowbarPicked_Up = false;
+        public bool generator_Turned_On = false;
+        public bool Security_Reset = false;
+        public bool generator_Stabilized = false;
+        public bool ID_Aquired = false;
+        public bool Food1_aquired  = false;
+        public bool Food2_aquired = false;
+        public bool Systems1_aquired = false;
+        public bool Systems2_aquired = false;
+        public bool Medical_aquired = false;
+    }
+}
---
+++ SilentSpace_EssentialsBuild/Assets/Scripts/UI/objectiveScreen.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace SilentSpace
+{
+    public class objectiveScreen : MonoBehaviour
+    {
+
+        public TextMeshProUGUI objectiveText;
+        private string[] objectives;
+        public int currentOBJ = 0; //this index starts counting from 0.
+
+
+
+     
+
+
+
+
+        void Update()
+        {
+
+
+           switch(currentOBJ)
+            {
+                case 0:
+                
+                    break;
+                
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+
+
+
+            }
+
+        }
+    }
+}
